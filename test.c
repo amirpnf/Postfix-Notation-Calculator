d0dbbe5fa6567728274c961aa50ec860a8afd3a6
amirh <pouyanfar.a2000@gmail.com>
2023-10-15 17:33:41 +0200

    some trial and error

--- test.c
+++ test.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <readline/readline.h>
 #include <readline/readline.h>
+#include <stdbool.h>
 
 #define MAX_SIZE 64
 
@@ -11,6 +12,10 @@ typedef struct stack {
     int top;
     int size;
 } Stack;
+
+void initialize_Stack(Stack* stack) {
+    stack->top = -1;
+}
 
 void print_Top(Stack stack) {
     if(stack.size > 0) {
@@ -29,27 +34,70 @@ void push(Stack* stack, int number) {
 }
 
 int pop(Stack* stack) {
-    if(stack->top >= 0) {
+      if(stack->top >= 0) {
         return stack->data[(stack->top)--];
     } else {
         fprintf(stderr, "There's nothing in the stack \n");
+        return 0;
     }
+}
+
+bool isNumerical(char* str) {
+    if(str == NULL || *str == '\0') {
+        return false;
+    }
+
+    int i = 0;
+    if(str[0] == '_') {
+        if(str[1] == '\0')
+            return false;
+        i = 1;    
+    }
+
+    for(; str[i] != '\0'; i++) {
+        if(str[i] < '0' || str[i] > '9')
+            return false;
+    } 
+    return true;
 }
 
 int main(int argc, char* argv[]) {
 
-    char* array = (char*)malloc(sizeof(char) * MAX_SIZE);
-    if(!array) {
-        fprintf("Memory allocation failed");
+    char* input;
+    Stack* stack;
+    stack = (Stack*)malloc(sizeof(Stack) * MAX_SIZE);
+    if(!stack) {
+        fprintf(stderr, "stack allocation failed \n");
         exit(1);
     }
-
-    rl_initialize();
-    char* command = (char*)malloc(sizeof(char) * 128);
+    
     while(1) {
-        char* command = readline("Enter something : ");
-        printf("%s \n", command);
+        input = readline("> ");
+        if(!input)
+            break;
+        if(strlen(input) == 0) {
+            free(input);
+            continue;
+        }    
+        char* token = strtok(input, " ");
+        while(token) {
+            if(isNumerical(token)) {
+                push(stack, atoi(token));
+            }
+            else {
+                switch (token[0])
+                {
+                case 'p':
+                    print_Top(*stack);
+                    break;
+                
+                case 'q':
+                    exit(0);
+                }
+            }
+        }
     }
-    free(command);
+    
     return 0;
+    
 }
